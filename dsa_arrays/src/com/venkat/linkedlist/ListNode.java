7ddb1e2349d67e8d0dcb0698e78d55292ca17e02
Venkat <venkata.marni@tradeleaves.com>
2026-04-08 13:01:43 +0530

    Single Linked List

---
+++ dsa_arrays/src/com/venkat/linkedlist/ListNode.java
@@ -0,0 +1,16 @@
+package com.venkat.linkedlist;
+
+public class ListNode {
+	public int value;
+	public ListNode next;
+	
+	public ListNode() {}
+	
+	public ListNode(int value) {
+		this.value = value;
+	}
+	public ListNode(int value, ListNode next) {
+		this.value = value;
+		this.next = next;
+	}
+}
---
+++ dsa_arrays/src/com/venkat/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,116 @@
+package com.venkat.linkedlist;
+
+public class SinglyLinkedList {
+	public Node head;
+	public Node tail;
+	public int size;
+	//create
+	public Node createSinglyLinkedList(int nodeValue) {
+		head = new Node();
+		Node node = new Node();
+		node.value = nodeValue;
+		node.next = null;
+		head = node;
+		tail = node;
+		size = 1;
+		return head;
+	}
+	//insertion
+	public void insertSLL(int nodeValue, int location) {
+		Node node = new Node();
+		node.value = nodeValue;
+		if(head == null) {
+			System.out.println("No SingleListExists");
+			createSinglyLinkedList(nodeValue);
+			return;
+		}else if(location == 0) {
+			node.next = head;
+			head = node;
+		}else if(location >= size) {
+			node.next = null;
+			tail.next = node;
+			tail = node;
+		}else {
+			Node tempNode = head;
+			int index = 0;
+			while(index < location -1) {
+				tempNode = tempNode.next;
+				index++;
+			}
+			Node nextNode = tempNode.next;
+			tempNode.next = node;
+			node.next = nextNode;
+		}
+		size++;
+	}
+	
+	//traversal
+	public void traverse() {
+		if(head == null) {
+			System.out.println("No Linked List Exists!");
+			return;
+		}else {
+			Node tempNode = head;
+			for(int i =0; i<size; i++) {
+				System.out.print(tempNode.value);
+				if(i != size -1) {
+				System.out.print("->");
+				}
+				tempNode = tempNode.next;
+			}
+			
+		}
+		System.out.println("\n");
+	}
+	
+	//search
+	public boolean search(int nodeValue) {
+		Node tempNode = head;
+		for(int i =0; i < size; i++) {
+			if(tempNode.value == nodeValue) {
+				System.out.println("Node value Found at index"+ i);
+				return true;
+			}
+			tempNode = tempNode.next;
+		}
+		System.out.println("Node not found! ");
+		return false;
+	}
+	
+	//Deletion
+	public void deleteNode( int location) {
+		Node node = head;
+		if(location == 0) {
+			node.next = head;
+			node= null;
+			size--;
+		}else if(location >= size) {
+			Node tempNode = head;
+			for(int i = 0; i < size -1; i++) {
+				tempNode = tempNode.next;
+			}
+			if(tempNode == head) {
+				tail = head = null;
+				size--;
+				return;
+			}
+			tempNode.next = null;
+			tail = tempNode;
+			size--;
+		}else {
+			Node tempNode = head;
+			for(int i =0; i<location-1; i++) {
+				tempNode = tempNode.next;
+			}
+			tempNode.next = tempNode.next.next;
+			size--;
+		}
+	}
+	
+	//Delete Entire List
+	public void deleteSLL() {
+		head = null;
+		tail = null;
+		System.out.println("The SLL deleted successfully");
+	}
+}

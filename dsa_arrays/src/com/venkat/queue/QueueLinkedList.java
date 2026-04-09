68182c09170754c909e05f60d4af38de24942f25
venkat <venkatthekiller@gmail.com>
2026-04-10 00:24:30 +0530

    Queue using LinkedList

---
+++ dsa_arrays/src/com/venkat/queue/QueueLinkedList.java
@@ -0,0 +1,54 @@
+package com.venkat.queue;
+
+public class QueueLinkedList {
+	LinkedList linkedList;
+
+	public QueueLinkedList(int value) {
+		linkedList = new LinkedList();
+		linkedList.createSinglyLinkedList(value);
+	}
+
+	// isEmpty
+	public boolean isEmpty() {
+		if (linkedList.head == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// enqueue()
+	public void enqueue(int value) {
+		linkedList.insertInLinkedList(value, linkedList.size);
+	}
+
+	// deQueue
+	public int dequeue() {
+		if (isEmpty()) {
+			System.out.println("Unable to delete the queue it is empty");
+			return -1;
+
+		} else {
+			int front = linkedList.head.value;
+			linkedList.deletionOfNode(0);
+			return front;
+		}
+	}
+
+	// peek
+	public int peek() {
+		if(isEmpty()) {
+			System.out.println("The Queue is Empty");
+			return -1;
+		}else {
+		return linkedList.head.value;
+		}
+	}
+
+	// delete
+	public void delete() {
+		linkedList.head = null;
+		linkedList.tail = null;
+		linkedList = null;
+	}
+}
